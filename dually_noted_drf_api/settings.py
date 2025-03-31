f134322e192d36c2a422d20c21c73d14cb8984c1
Oliver Griffin <olivergriffin2991@gmail.com>
2025-03-31 21:07:29 +0100

    Further deployment changes

--- dually_noted_drf_api/settings.py
+++ dually_noted_drf_api/settings.py
@@ -65,7 +65,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dually-noted.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
