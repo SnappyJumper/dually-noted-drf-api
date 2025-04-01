15a3b636c2531121ce9f8e3ab53eb624e6eebaa6
Oliver Griffin <olivergriffin2991@gmail.com>
2025-04-01 18:55:57 +0100

    Updated heroku address on allowed hosts

--- dually_noted_drf_api/settings.py
+++ dually_noted_drf_api/settings.py
@@ -65,7 +65,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'dually-noted-f734097d9d34.herokuapp.com/',
+]
 
 
 # Application definition
