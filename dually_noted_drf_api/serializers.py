adfed88897056711338448d28a2509ab4b7eebb0
Oliver Griffin <olivergriffin2991@gmail.com>
2025-04-09 11:43:45 +0100

    Tweaked the serializers to fix display profile issue

--- dually_noted_drf_api/serializers.py
+++ dually_noted_drf_api/serializers.py
@@ -3,10 +3,13 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    profile_id = serializers.ReadOnlyField(source='userprofile.id')
+    profile_picture = serializers.ReadOnlyField(
+        source='userprofile.profile_picture.url'
+    )
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'profile_id', 'profile_picture'
         )
+
